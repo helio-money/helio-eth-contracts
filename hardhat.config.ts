d7945a46ff4b0647ec035e4b5a0853d810fb4024
Razorback <raz.w@binance.com>
2023-12-20 17:01:51 +0400

    chore: set allowUnlimitedContractSize to `true` to make test cases pass

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,55 +4,56 @@ import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
-      viaIR: true,
-    },
-    compilers: [
-      {
+    solidity: {
         version: "0.8.19",
         settings: {
-          outputSelection: {
-            "*": {
-              "*": ["storageLayout"],
+            optimizer: {
+                enabled: true,
+                runs: 1000,
             },
-          },
+            viaIR: true,
         },
-      },
-    ],
-  },
-  networks: {
-    hardhat: {
-      chainId: 1,
-      forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 18071507,
-      },
+        compilers: [
+            {
+                version: "0.8.19",
+                settings: {
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            },
+        ],
     },
-    mainnet: {
-      chainId: 1,
-      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.DEPLOYER_KEY as string],
+    networks: {
+        hardhat: {
+            chainId: 1,
+            allowUnlimitedContractSize: true,
+            forking: {
+                url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+                blockNumber: 18071507,
+            },
+        },
+        mainnet: {
+            chainId: 1,
+            url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.DEPLOYER_KEY as string],
+        },
+        goerli: {
+            chainId: 5,
+            url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.DEPLOYER_KEY as string],
+        },
+        sepolia: {
+            chainId: 11155111,
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.DEPLOYER_KEY as string],
+        },
     },
-    goerli: {
-      chainId: 5,
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.DEPLOYER_KEY as string],
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
     },
-    sepolia: {
-      chainId: 11155111,
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.DEPLOYER_KEY as string],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
 };
 
 export default config;
